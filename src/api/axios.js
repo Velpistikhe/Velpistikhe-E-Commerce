ae022167ae6597969afaf69c0d45122f77038c63
Velpistikhe <velpistikhe@gmail.com>
2025-08-29 22:54:51 +0700

    change base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://restful-api-production-7859.up.railway.app/api/v1/",
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: "https://restful-api-production-7859.up.railway.app/api/v1/",
   timeout: 10000,
   withCredentials: true,
 });
