cb748f74d5a66bdaa3e3daa06ec29851cf4dbd61
Velpistikhe <velpistikhe@gmail.com>
2025-08-30 10:21:14 +0700

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "https://restful-api-production-7859.up.railway.app/api/v1/",
   timeout: 10000,
   withCredentials: true,
 });
